c94e91f1294602cc2dd0e6d897f074d5f3e97431
Matias Bensan Araya <mbensan@gmail.com>
2023-11-15 22:28:23 -0300

    Ejercicios de Condcionales y Ciclos

--- js/siempre_gana/script.js
+++ js/siempre_gana/script.js
@@ -1,7 +1,10 @@
 // Este es un programa que nunca pierde
 // 1. Le preguntamos al usuario que elija un número
 const bienvenida = 'Hola, escribe un número'
-const eleccion_usuario = prompt(bienvenida)
+let eleccion_usuario = prompt(bienvenida)
+
+// 1.5 Transformar la "eleccion_usuario" a número entero
+eleccion_usuario = parseInt(eleccion_usuario)
 
 // 2. Elegimos un número 1 mayor
 const eleccion_programa = (eleccion_usuario + 1)
