2f8f8a58de994d5068cf5548d29aece8398a2ed6
Matias Bensan Araya <mbensan@gmail.com>
2023-12-04 20:29:49 -0300

    Ejemplo de Pokemon

---
+++ dom/intro_hhtp.md
@@ -0,0 +1,5 @@
+- Programa que realiza peticiones HTTP (GET, POST, PUT, DELETE)
+- Renderer (dibuja la página HTML)
+- Motor de Javascript
+
+JSON => Javascript Object Notation
---
+++ dom/pokemon/script.js
@@ -0,0 +1,41 @@
+function add_pokemon_list () {
+  // 1. Nos traemos el ROW
+  const pokemones_row = document.querySelector('#pokemones')
+  // 2. Iteramos desde el 1 al 40, agregando las respectivas imagenes a un string vació
+  let pokemones_html = ''
+  for (let i=1; i<=40; i++) {
+    pokemones_html += `
+      <div class="col-3 p-3 poke-col">
+        <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${i}.png" class="w-100 d-block" alt="pomeon imagen" onclick="mostrar_detalle(${i})">
+      </div>
+    `
+  }
+
+  // 3. Agrego el string formado al tag pokemones_row
+  pokemones_row.innerHTML = pokemones_html
+}
+
+add_pokemon_list()
+
+async function mostrar_detalle(pokemon_id) {
+  // 0. Nos traemos las etiquetas a utilizar
+  const poke_img = document.querySelector('#poke-img')
+  const poke_nombre = document.querySelector('#poke-nombre')
+  const poke_tipo = document.querySelector('#poke-tipo')
+  const poke_peso = document.querySelector('#poke-peso')
+
+
+  // 1. Vamos a buscar los datos del pokemon solicitado
+  let resp = await fetch('https://pokeapi.co/api/v2/pokemon/' + pokemon_id)
+
+  // 2. Desempaquetamos los datos
+  const pokemon = await resp.json()
+
+
+
+  poke_nombre.innerHTML = pokemon.name
+  poke_tipo.innerHTML = pokemon.types[0].type.name
+  poke_peso.innerHTML = pokemon.weight
+  console.log(pokemon);
+
+}
